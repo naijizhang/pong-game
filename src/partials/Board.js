ebdd00102c04968c19340c90d0230fe35c7896af
Naiji Zhang <naijizhang@gmail.com>
2019-02-07 15:24:25 -0800

    Add Board and Paddle(Players)

---
+++ src/partials/Board.js
@@ -0,0 +1,29 @@
+import { SVG_NS } from "../settings";
+export default class Board {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  render(svg) {
+    //the board background
+    let rect = document.createElementNS(SVG_NS, "rect");
+    rect.setAttributeNS(null, "width", this.width);
+    rect.setAttributeNS(null, "height", this.height);
+    rect.setAttributeNS(null, "color", "#353535");
+    // this.gameElement.appendChild(rect);
+
+    // dashed dividing line
+    let line = document.createElementNS(SVG_NS, "line");
+    line.setAttributeNS(null, "stroke-dasharray", "20, 10");
+    line.setAttributeNS(null, "x1", this.width/2);
+    line.setAttributeNS(null, "y1", this.height);
+    line.setAttributeNS(null, "x2", this.width/2);
+    line.setAttributeNS(null, "y2", 0);
+    line.setAttributeNS(null, "stroke", "white");
+    line.setAttributeNS(null, "stroke-width", 3);
+    // this.gameElement.appendChild(line);
+
+    svg.appendChild(rect);
+    svg.appendChild(line);
+  }
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,29 @@
+import { SVG_NS } from "../settings";
+import Board from "./Board"
+import Paddle from "./Paddle";
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
-
-		// Other code goes here...
+    this.gameElement = document.getElementById(this.element);
+    this.board=new Board(this.width,this.height);
+    this.paddle1= new Paddle(this.height,8,56,484,100); 
+    this.paddle2= new Paddle(this.height,8,56,20,100); 
+  
   }
 
   render() {
-		// More code goes here....
+    this.gameElement.innerHTML = "";
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+
+    // rendering all elements inside SVG
+    this.board.render(svg);
+    this.paddle1.render(svg);
+    this.paddle2.render(svg);
   }
 }
