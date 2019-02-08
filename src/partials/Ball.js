3fdd31a45bd4246e58a3536075e4f8fb14375aa2
Naiji Zhang <naijizhang@gmail.com>
2019-02-07 20:45:35 -0800

    Add Ball Movement and Wallision

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -5,19 +5,37 @@ export default class Ball {
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
     this.direction = 1;
+
+    this.reset();
     this.vy = Math.floor(Math.random() * 10 - 5);
+    while (!this.vy) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
+    }
     this.vx = this.direction * (6 - Math.abs(this.vy));
   }
   reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
   }
+  wallCollision() {
+    //top   or  bottom
+    if (this.y - this.radius < 0 || this.y + this.radius > this.boardHeight) {
+      this.vy *= -1;
+    }
+    // left or right
+
+    if (this.x - this.radius < 0 || this.x + this.radius > this.boardWidth) {
+      this.vx *= -1;
+    }
+
+    //left
+  }
   render(svg) {
-    //<circle r="8" cx="256" cy="128" fill="white" />
+    this.wallCollision();
     let circle = document.createElementNS(SVG_NS, "circle");
     circle.setAttributeNS(null, "r", this.radius);
-    circle.setAttributeNS(null, "cx", this.boardWidth / 2);
-    circle.setAttributeNS(null, "cy", this.boardHeight / 2);
+    circle.setAttributeNS(null, "cx", (this.x += this.direction * this.vx));
+    circle.setAttributeNS(null, "cy", (this.y += this.direction * this.vy));
     circle.setAttributeNS(null, "fill", "white");
     svg.appendChild(circle);
   }
